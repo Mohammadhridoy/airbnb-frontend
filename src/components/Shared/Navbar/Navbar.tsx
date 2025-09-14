6b408e036576bb22e2e89d2bd2c3969026fa4325
Mohammadhridoy <mdridoykha019@gmail.com>
2025-09-15 00:06:31 +0600

    fix bug and design navbar center

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,13 +1,33 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import logo from '../../../../public/airbnb.png'
-import { Button } from '@/components/ui/button';
 import { NavbarHost } from '@/components/Modules/NavbarHost';
 import { NavbarTranslator } from '@/components/Modules/NavbarTranslator';
 import { NavbarDropdownMenu } from '@/components/Modules/NavbarDropdownMenu';
+import Lottie from "lottie-react";
+import  homeGif from "../../../../public/Home.json"  
+import experienceGif from "../../../../public/Hot Air Balloon.json"
+import servicesGif from "../../../../public/ServisHero Loading.json"
+import { usePathname } from 'next/navigation';
+import { useInView } from 'react-intersection-observer';
 
 const Navbar = () => {
+    const pathname = usePathname()
+
+    const {ref, inView} = useInView({
+        triggerOnce: true,
+        threshold: 0.3
+    })
+
+    const menuItems = [
+        { name:"Homes", href:"/", animation: homeGif},
+         { name:"Experiences", href:"/experiences", animation: experienceGif},
+         {name:"Services", href:"/services", animation:servicesGif}
+
+    ]
+
     return (
         <div className='w-full border-b'>
             {/* Navbar upper section */}
@@ -28,11 +48,54 @@ const Navbar = () => {
                </div>
             
             {/* Navbar center */}
-           <div className=' text-center' >
-           <div >
+           <div className=' grid grid-cols-3 gap-3 py-4' >
+          
+            {
+              menuItems.map((item, index)=>{
+                const isActive = pathname === item.href
+
+                return(
+                 <Link 
+                 ref={ref} 
+                 key={index}
+                 href={item.href} 
+                 className= {
+                    ` relative  flex  flex-row justify-center items-center 
+                 
+                 `
+                 }
+                 >
+             
+             {inView && (
+                   <Lottie
+                animationData={item.animation}
+                loop={false}
+                className={`w-14 h-14
+                ${isActive?" ":"hover:scale-150 transition-transform"}
+                `}
+                />
+             )}
+
+                
+              <h1 className={
+                `text-base font-semibold mt-2 
+              hover:text-gray-400
+              ${isActive?"text-black": "text-gray-400"}
+              `
+              }>{item.name}</h1>
+              <span
+              className={`
+                absolute bottom-0 left-0 h-[2px] bg-black transition-all 
+              duration-200 ease-in-out 
+                ${isActive? 'w-full': 'w-0 hover:w-full'}
+                `}
+              />
               
-         
-           </div>
+              </Link>
+              )
+              })
+            }
+          
            </div>
 
 
